26d4d18fc03d68afb88fb831e7765ef4ea883ec8
Joe Hellerstein <jmh@berkeley.edu>
2011-07-11 11:31:33 -0400

    route sets of items through Eddy and Stems

--- lib/crocus/eddy.rb
+++ lib/crocus/eddy.rb
@@ -1,15 +1,20 @@
 require 'crocus/elements'
 
 class Blossom
-  class EddyItem
-    attr_accessor :item, :ready, :done, :source_name, :source_id
-    def initialize(item_in, source_name_in, source_id_in, stems_in)
-      @item = item_in
+  class EddyItemSet
+    # and ItemSet is a set of (possibly partially empty) result tuple-arrays, which
+    # share the same sources and ready/done status
+    attr_accessor :items, :ready, :done, :source_name, :source_id
+    def initialize(itemset_in, source_name_in, source_id_in, stems_in)
+      @items = itemset_in
       @source_name = source_name_in
       @source_id = source_id_in
       @ready = 2**source_id_in # ready to route to self
       # puts "@all_on = #{@all_on}"
       @done = 0
+    end
+    def each
+      @items.each
     end
   end
   
@@ -24,29 +29,37 @@ class Blossom
       super(name, arity, [], &blk)
     end
     
-    def insert(item, key)      
-      if item.source_name == @name
+    def insert(itemset, key_cols)      
+      if itemset.source_name == @name
         # insert into itemset and call @blk
         # puts "inserting #{item.item.inspect} into Stem #{@name}"
         # key = @key.map{|i| item.item[item.source_id][i]}
-        @items[key] ||= []
-        @items[key] << [item.item[@my_id]] 
-        @blk.call(item)
+        itemset.items.each do |item|
+          subitem = item[itemset.source_id]
+          key = key_cols.map{|i| subitem[i]}          
+          @items[key] ||= []
+          @items[key] << subitem
+        end
+        @blk.call(itemset)
       else
         # find matches and route resulting concatenation via calling @blk
         # puts "looking for key #{key.inspect} in #{@name}"
-        matches = @items[key]
-        if matches.nil?
-          # puts "no matches for key #{key.inspect} in #{@name}"
-          @blk.call(nil)
-        else 
-          # origitemitem = item.item[@my_id].clone
-          matches.each do |i|
-            item.item.delete_at @my_id
-            item.item.insert(@my_id,i.flatten)
-            @blk.call(item)
+        newitems = []
+        itemset.items.each do |item|
+          subitem = item[itemset.source_id]
+          key = key_cols.nil? ? [] : key_cols.map{|i| subitem[i]} 
+          matches = @items[key]
+          unless matches.nil?
+            matches.each do |i|
+              item.delete_at @my_id
+              item.insert(@my_id,i.flatten)
+              newitem = item.clone
+              newitems << item.clone
+            end
           end
         end 
+        itemset.items = newitems
+        @blk.call(itemset)
       end
     end
       
@@ -124,13 +137,13 @@ class Blossom
     def insert(item, source) 
       # convert inbound singleton into outbound format
       item = @inputs.map.with_index{|inp, i| source.name == id_to_name[i] ? item : []}
-      item = EddyItem.new(item, source.name, name_to_id[source.name], @stems)
+      itemset = EddyItemSet.new([item], source.name, name_to_id[source.name], @stems)
       # puts "created EddyItem #{item.inspect}"
       # and route
-      route(item)
+      route(itemset)
     end
     
-    def choose_route(item)
+    def choose_route(itemset)
       # route to self first
        # selfbit = 2**@name_to_id[item.source_name]
        # n = nil
@@ -144,7 +157,7 @@ class Blossom
        # but for now all we'll do is find the first stem that's ready
        n = nil
        (0..@id_to_name.length-1).each do |i| 
-         if item.ready & 2**i != 0
+         if itemset.ready & 2**i != 0
            n = @stems[@id_to_name[i]][0]
            raise "n is nil, #{@id_to_name[i]}" if n.nil?
            break
@@ -153,39 +166,41 @@ class Blossom
        return n
     end
     
-    def route(item)
-      return if item.nil?
-      raise "item from unknown source #{item.source_name}" if @name_to_id[item.source_name].nil?
+    def route(itemset)
+      return if itemset.nil?
+      raise "item from unknown source #{itemset.source_name}" if @name_to_id[itemset.source_name].nil?
       # puts "item.done = #{item.done}, @inputs.length = #{@inputs.length}"
-      if item.done == @all_on
-        raise "orphaned stuff in ready: #{item.ready.inspect}" unless item.ready == 0
-        return @blk.call(item.item)
+      if itemset.done == @all_on
+        raise "orphaned stuff in ready: #{itemset.ready.inspect}" unless itemset.ready == 0
+        itemset.items.each {|item| @blk.call(item)}
+        return 
       end
       # puts "routing #{item.item.inspect} from #{item.source_name}"
       
       # if nothing's ready, must be time to cross-product with something
-      item.ready = @all_on - item.done if item.ready == 0
+      itemset.ready = @all_on - itemset.done if itemset.ready == 0
       # choose dest
-      n = choose_route(item)
-      if item.source_id != n.my_id
+      n = choose_route(itemset)
+      if itemset.source_id != n.my_id
         entry = @name_to_preds[n.name]
-        key = entry.first[1] unless entry.nil? # so far we only handle on a single join pred!
-        key = key.nil? ? [] : key.map{|i| item.item[item.source_id][i]}
-        # puts "routing #{item.item.inspect} from #{item.source_name} to Stem #{n.name} with lookup key #{key}"
+        key_cols = entry.first[1] unless entry.nil? # so far we only handle on a single join pred!
+        # key = key_cols.nil? ? [] : key.map{|i| item.item[item.source_id][i]}
+        # puts "routing #{item.item.inspect} from #{item.source_name} to Stem #{n.name} with lookup key #{key_cols}"
       else
-        key = n.key.map{|i| item.item[item.source_id][i]}
-        # puts "routing #{item.item.inspect} from #{item.source_name} to Stem #{n.name} with insert key #{key}"
+        key_cols = n.key
+        # key = key_cols.map{|i| item.item[item.source_id][i]}
+        # puts "routing #{item.item.inspect} from #{item.source_name} to Stem #{n.name} with insert key #{key_cols}"
       end
 
       # flip the bits to what they should be AFTER this insert
       preds = name_to_preds[n.name] 
-      item.ready -= n.my_bit
+      itemset.ready -= n.my_bit
       unless preds.nil?
-        preds.each {|i| item.ready += i[0] if (item.ready & i[0] == 0) and (item.done & i[0] == 0)}
+        preds.each {|i| itemset.ready += i[0] if (itemset.ready & i[0] == 0) and (itemset.done & i[0] == 0)}
       end
-      item.done += n.my_bit 
+      itemset.done += n.my_bit 
               
-      n.insert(item, key)
+      n.insert(itemset, key_cols)
     end
   end
 end
