5807212103bc007e7b3e91d086f540e44bb83bcf
Joe Hellerstein <jmh@berkeley.edu>
2011-07-13 15:42:14 -0400

    flush batches through join

--- lib/crocus/join.rb
+++ lib/crocus/join.rb
@@ -6,12 +6,21 @@ class Crocus
       super(name, arity, *sources_in, &blk)
       @items = [{}, {}]
       @source_names = sources_in.map{|s| s.name}
+      @input_bufs = [[],[]]
       @keys = keys_in
     end
   
     def insert(item, source)
       offset = (@source_names[0] == source.name) ? 0 : ((@source_names[1] == source.name) ? 1 : nil)
       raise "item #{item} inserted into join from unknown source #{source.name}" if offset.nil?
+      buf = @input_bufs[offset]
+      buf << item
+      if (buf.length >= BUFSIZE)
+        flush_buf(buf, offset)
+      end
+    end
+    
+    def insert_item(item, offset)
       key = @keys[offset].map{|k| item[k]}
       #build
       # puts "building #{item} into @source[#{offset}] on key #{key}"
@@ -26,6 +35,17 @@ class Crocus
         result[1-offset] = m
         @blk.call(result.flatten)
       end unless matches.nil?
+    end
+    
+    def flush_buf(buf, offset)
+      buf.each do |item|
+        insert_item(item, offset)
+      end
+    end
+    def flush
+      @input_bufs.each_with_index do |buf, offset| 
+        flush_buf(buf, offset) if buf.length > 0
+      end
     end
   end
 end
